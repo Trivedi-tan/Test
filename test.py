fc5b1119343748f897872b025cf69f6edababfcf
Tanmay Trivedi <tanmaytrivedi@Tanmays-MacBook-Air.local>
2024-01-13 02:57:46 +0530

    commit

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
 print("this is a test code")
 print("to check")
+
+#1. init byt git init
+#2. git remote origin with url project
+#3. git add .
+#4. git commit -m "msg"
+#5. git git push origin
+#6. git push --
