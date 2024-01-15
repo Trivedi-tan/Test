d388dae6f34a5f32d88f6875652efa3ce9f86ffb
Tanmay Trivedi <tanmaytrivedi@Tanmays-MacBook-Air.local>
2024-01-15 19:49:07 +0530

    checking rollback

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 print("this is a test code")
 print("to check")
+print("checkimg rollback")
 
 #1. init byt git init
 #2. git remote origin with url project
 #3. git add .
 #4. git commit -m "msg"
 #5. git git push origin
-#6. git push --
+#6. git push --s
